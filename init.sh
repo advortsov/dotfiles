804f60466babad0452b76e4333aea9f5c38791f1
advortsov <alxdvcv@gmail.com>
2020-07-12 20:00:29 +0400

    change init

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo apt-get -y update &&
-sudo apt-get -y install ranger tmux git chromium-browser &&
+sudo apt-get -y install nodejs ranger tmux git chromium-browser &&
 git config --global user.email "alxdvcv@gmail.com" &&
 git config --global user.name "advortsov" &&
 cd ~ &&
 git clone https://github.com/advortsov/dotfiles.git &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+export VIMINIT='source ~/dotfiles/.vimrc' &&
 vim +PlugInstall +qall &&
-VIMINIT='source ~/dotfiles/.vimrc' &&
 echo Success
