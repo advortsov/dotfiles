e284846f4647c574ff7fef777f0045b93ead78ed
advortsov <alxdvcv@gmail.com>
2020-07-13 12:59:24 +0400

    add vpn and java dev init scripts

--- init.sh
+++ init.sh
@@ -1,11 +1,14 @@
 sudo apt-get -y update &&
-sudo apt-get -y install nodejs ranger tmux git chromium-browser &&
+sudo apt-get -y install python3-pip python3-venv nodejs ranger tmux git chromium-browser &&
+pip3 install virtualenv virtaulenvwrapper &&
+xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode Virtual1 "1920x1080_60.00" &&
 git config --global user.email "alxdvcv@gmail.com" &&
 git config --global user.name "advortsov" &&
 cd ~ &&
 git clone https://github.com/advortsov/dotfiles.git &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
-export VIMINIT='source ~/dotfiles/.vimrc' &&
+echo "VIMINIT='source ~/dotfiles/.vimrc'" >> ~/.bashrc
 vim +PlugInstall +qall &&
 echo Success
---
+++ java-dev.sh
@@ -0,0 +1,6 @@
+sudo apt-get update &&
+sudo apt-get install openjdk-11-jdk maven &&
+mv ~/Downloads/settings.xml ~/.m2 &&
+sudo snap install intellij-idea-community --classic
+
+
