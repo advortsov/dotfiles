65fab7a897086bfb2d8f41aeb5de7415be1deee8
advortsov <a.dvortsov@iteco-inno.ru>
2020-09-16 23:10:57 +0300

    +

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,5 +6,9 @@ ln -sf ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf &&
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config &&
 ln -sf ~/dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc &&
 ln -sf ~/dotfiles/.bashrc ~/.bashrc &&
-ln -sf ~/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -sf ~/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh &&
+ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf &&
+ln -sf ~/dotfiles/kitty/kitty-themes/themes/Solarized_Dark_Higher_Contrast.conf ~/.config/kitty/kitty-themes/themes/Solarized_Dark_Higher_Contrast.conf &&
+ln -sf ~/dotfiles/kitty/theme.conf ~/.config/kitty/theme.conf &&
+ln -sf ~/dotfiles/rofi/config ~/.config/rofi/config
 
